86328df9e1578b1b41805f35884df2e17b0a74f1
dilshergrewal <grewal.sher@gmail.com>
2022-08-28 22:32:26 +0530

    Update Team2Application.java

--- src/main/java/team2/Team2Application.java
+++ src/main/java/team2/Team2Application.java
@@ -2,6 +2,7 @@ package team2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"team2.controller"})
