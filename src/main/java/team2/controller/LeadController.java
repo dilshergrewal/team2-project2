778b4d99b5681106617c33e7df294b9bc10d7f02
dilshergrewal <grewal.sher@gmail.com>
2022-08-28 20:48:25 +0530

    Update LeadController.java

--- src/main/java/team2/controller/LeadController.java
+++ src/main/java/team2/controller/LeadController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LeadController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/view")
 	public String saveLead() {
 		
 		return "index";
